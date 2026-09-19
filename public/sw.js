df8bea630dd61493342832882ea63b7dfc94ee62
vercelsistemasfag <vercelsistemas@gmail.com>
2026-09-19 20:42:25 +0000

    Aplica banner aprovado

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'busca-preco-nh-v15-green-banner'
-const SHELL = ['/', '/style.css', '/assets/busca-preco-logo.svg', '/app.js', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png']
+const CACHE = 'busca-preco-nh-v17-approved-banner'
+const SHELL = ['/', '/style.css', '/assets/banner-encartes-aprovado.png', '/assets/busca-preco-logo.svg', '/app.js', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png']
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting()})
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()})
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match('/'))))})
